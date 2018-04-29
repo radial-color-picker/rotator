65503e76773acb778d749dad0df42f063b92e367
Rosen Kanev <rkunev@gmail.com>
2018-04-29 22:36:10 +0300

    build: Transpile es2015 syntax with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,36 @@
 import butternut from 'rollup-plugin-butternut';
+import babel from 'rollup-plugin-babel';
 
 export default [
     {
         input: 'src/main.js',
-        output: { name: 'rotator', file: 'dist/rotator.umd.js', format: 'umd' }
+        output: { name: 'rotator', file: 'dist/rotator.umd.js', format: 'umd' },
+        plugins: [
+            babel({
+                exclude: ['node_modules/**'],
+            }),
+        ]
     },
     {
         input: 'src/main.js',
         output: { name: 'rotator', file: 'dist/rotator.umd.min.js', format: 'umd' },
-        plugins: [butternut()]
+        plugins: [
+            babel({
+                exclude: ['node_modules/**'],
+            }),
+            butternut()
+        ]
     },
     {
         input: 'src/main.js',
         output: [
             { file: 'dist/rotator.cjs.js', format: 'cjs' },
             { file: 'dist/rotator.esm.js', format: 'es' }
+        ],
+        plugins: [
+            babel({
+                exclude: ['node_modules/**'],
+            })
         ]
     }
 ];
