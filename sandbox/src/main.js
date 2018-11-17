7f03cb1bee70b5ba1d2a65827043ebd4b4632c6a
Rosen Kanev <rkunev@gmail.com>
2018-11-17 23:57:16 +0200

    build(sandbox): Add reset button

--- sandbox/src/main.js
+++ sandbox/src/main.js
@@ -1,11 +1,17 @@
 import Rotator from '../dist/rotator.esm.js';
 
-const tip = document.querySelector('#tip-value');
+const tip = document.querySelector('#tip');
+const tipValue = document.querySelector('#tip-value');
 const protractor = document.querySelector('#protractor');
 const r = new Rotator(protractor, {
     onRotate,
 });
 
 function onRotate(angle) {
-    tip.innerText = `${(360 - angle).toFixed(1)}°`;
+    tipValue.innerText = `${(360 - angle).toFixed(1)}°`;
 }
+
+tip.addEventListener('click', event => {
+    r.angle = 0;
+    tipValue.innerText = '0°';
+});
